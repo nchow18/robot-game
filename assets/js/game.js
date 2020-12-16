02c714e03974f8ecaca65aaaf403d046f6dfbd76
Nathan Chow <emailme@nathanchow.ca>
2020-12-16 16:15:57 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,6 +75,13 @@ var fight = function(enemyName) {
   }
 };
 
+var startGame = function() {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  var pickedEnemyName = enemyNames[i];
 
 
 for (var i = 0; i < enemyNames.length; i++) {
@@ -103,3 +110,32 @@ if (playerHealth > 0) {
   enemyHealth = 50;
   fight(pickedEnemyName);
 }
+
+//startGame();
+endGame();
+}
+
+// function to end the entire game
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+  // ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+
+}
+
+startGame();
